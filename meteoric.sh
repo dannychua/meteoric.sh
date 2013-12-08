0d3b5081f9ffddea317db1d3b581383f459af82f
Danny <dannychua@Dannys-MacBook-Air.local>
2013-12-08 15:11:12 -0500

    Updated node ppa to use non-legacy
    Removed npm from apt-get, newer versions of node comes with npm

--- meteoric.sh
+++ meteoric.sh
@@ -58,10 +58,10 @@ fi
 
 SETUP="
 sudo apt-get install software-properties-common;
-sudo add-apt-repository ppa:chris-lea/node.js-legacy;
+sudo add-apt-repository ppa:chris-lea/node.js;
 sudo apt-get -qq update;
 sudo apt-get install git mongodb;
-sudo apt-get install nodejs npm;
+sudo apt-get install nodejs;
 node --version;
 sudo npm install -g forever;
 curl https://install.meteor.com | /bin/sh;
